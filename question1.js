9ded8672ac6e41810e8fc05854bdfa44c214df55
BrylDG <148348465+BrylDG@users.noreply.github.com>
2025-03-28 01:25:07 +0800

    question1.js

--- question1.js
+++ question1.js
@@ -3,4 +3,4 @@ function sumArray(num) {
   //using reduce method to add each elements within the array to get the total sum.
 }
 
-console.log(sumArray([1, 2, 3, 4, 5]));  //This should return 15 (1 + 2 + 3 + 4 + 5)
+console.log(sumArray([1, 2, 3, 4, 5]));  //This should return 15 (1 + 2 + 3 + 4 + 5).
